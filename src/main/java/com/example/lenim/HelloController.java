3a9f69b502998acd89fbec0fba4170ed93f62ee1
Ben-Casillas <98473776+Ben-Casillas@users.noreply.github.com>
2023-01-17 17:09:47 -0700

    Menus Start Up

--- src/main/java/com/example/lenim/HelloController.java
+++ src/main/java/com/example/lenim/HelloController.java
@@ -1,15 +1,25 @@
 package com.example.lenim;
 
+import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+        @FXML
+        private Button onePlayerBtn;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to LeNim");
-        System.out.println("Hello Comire");
-    }
+        @FXML
+        private Button twoPlayerBtn;
+
+        @FXML
+        void onePlayer(Event event) throws IOException {
+                ChangeScene.changeScene(event, "one-Player.fxml");
+        }
+
+        @FXML
+        void twoPlayer(Event event) throws IOException {
+                ChangeScene.changeScene(event, "two-Player.fxml");
+        }
 }
