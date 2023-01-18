7711033fedef187b95cd49357106a345f5054d49
LandonCobb <landoncobb123@gmail.com>
2023-01-18 14:00:56 -0700

    updated skeleton of game logic

--- src/main/java/com/example/lenim/GameInstance.java
+++ src/main/java/com/example/lenim/GameInstance.java
@@ -1,15 +1,37 @@
 package com.example.lenim;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class GameInstance {
-    private int gameTokens;
-    private Player[] players;
-    private int playerTurn;
+    private static ArrayList<Player> players = new ArrayList<>();
+    private static int playerTurn = 0;
+
+    public static void Start(){
+        boolean done = false;
+        do {
+            //set players
+            GenerateTokens();
+            if ((playerTurn % 2 == 0)){
+                //player 1 turn (players[(playerTurn % 2)].name)
+            } else {
+                //player 2 turn (players[(playerTurn % 2)].name)
+            }
+            //select token
+            done = CheckWin();
+            playerTurn++;
+
+        }while (!done);
+    }
 
 
-    public GameInstance(int gameTokens, int playerTurn) {
-        this.gameTokens = gameTokens;
-        this.players = new Player[2];
-        this.playerTurn = playerTurn;
+    public static void GenerateTokens(){
+        //generate random # tokens
+    }
+
+    public static boolean CheckWin(){
+        //if statement for player 1 winning
+        //if statement for player 2 winning
+        return false;
     }
 }
