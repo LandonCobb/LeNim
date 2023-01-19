fed52dd6ccca820cb0675053444b8f4a6867bf84
Ben-Casillas <98473776+Ben-Casillas@users.noreply.github.com>
2023-01-18 17:03:25 -0700

    Better UI, change scene working

--- src/main/java/com/example/lenim/HelloController.java
+++ src/main/java/com/example/lenim/HelloController.java
@@ -5,44 +5,55 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+
 import java.io.IOException;
 
 
 public class HelloController {
+    @FXML
+    private TextField errorMsg;
+    @FXML
+    private Button onePlayerBtn;
+    @FXML
+    private TextField player1Name;
+    @FXML
+    private Button start;
+    @FXML
+    private TextField player2Name;
+    @FXML
+    private Button twoPlayerBtn;
 
-        @FXML
-        private Button onePlayerBtn;
+    @FXML
+    void onePlayer(MouseEvent event) {
+        player1Name.setVisible(true);
+        start.setVisible(true);
+        player2Name.setVisible(false);
+        player1Name.setText("");
+        player2Name.setText("");
+    }
 
-        @FXML
-        private TextField player1Name;
+    @FXML
+    void twoPlayer(MouseEvent event) {
+        player1Name.setVisible(true);
+        start.setVisible(true);
+        player2Name.setVisible(true);
+        player1Name.setText("");
+    }
 
-        @FXML
-        private Button start;
+    @FXML
+    void proceed(Event event) throws IOException {
+        boolean validNames = false;
 
-        @FXML
-        private TextField player2Name;
-
-        @FXML
-        private Button twoPlayerBtn;
-        @FXML
-        void onePlayer(MouseEvent event) {
-                player1Name.setVisible(true);
-                start.setVisible(true);
-                player2Name.setVisible(false);
-                player1Name.setText("");
-                player2Name.setText("");
-        }
-
-        @FXML
-        void twoPlayer(MouseEvent event) {
-                player1Name.setVisible(true);
-                start.setVisible(true);
-                player2Name.setVisible(true);
-                player1Name.setText("");
-        }
-
-        @FXML
-        void proceed(Event event) throws IOException {
+        do {
+            if (player1Name.getText() != "" || player2Name.getText() != "") {
+                validNames = true;
                 ChangeScene.changeScene(event, "game-board.fxml");
+                }
+            else{
+                errorMsg.setText("Please ");
+            }
+            }
+            while (!validNames);
+
         }
-}
+    }
