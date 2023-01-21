08746d899f391ce5c8e5f4caaee1bc4b9a7e936b
Ben-Casillas <98473776+Ben-Casillas@users.noreply.github.com>
2023-01-21 14:35:43 -0700

    token.png change

--- src/main/java/com/example/lenim/HelloApplication.java
+++ src/main/java/com/example/lenim/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("game-setup.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Do Stuff!");
+        stage.setTitle("Le Nim");
         stage.setScene(scene);
         stage.show();
         //make fullscreen :D
