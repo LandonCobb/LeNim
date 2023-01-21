3be9d274f4cb8b2988d6c49b799e059688272f80
Ben-Casillas <98473776+Ben-Casillas@users.noreply.github.com>
2023-01-21 14:27:41 -0700

    something silly

--- src/main/java/com/example/lenim/HelloController.java
+++ src/main/java/com/example/lenim/HelloController.java
@@ -6,8 +6,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import java.io.IOException;
-
-
 public class HelloController {
 
         @FXML
